f59153daff177b2a0fd1522ce9d224edfa7650e4
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-05 17:04:41 +0700

    Url yang valid
    Memperbaiki url yang valid agar supaya dari push notif dibuka dengan url yang valid, diharapkan tidak membuka halaman tampilan 404.

--- service-worker.js
+++ service-worker.js
@@ -20,12 +20,18 @@ self.addEventListener('push', function(event) {
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
+    const githubUrl = "https://aflacake.github.io/piodrink/";
+
     event.waitUntil(
-        clients.matchAll({ type: 'window' }).then(clientList => {
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
             for (const client of clientList) {
-                if (client.url === '/' && 'focus' in client) return client.focus();
+                if (client.url === githubUrl && 'focus' in client) {
+                    return client.focus();
+                }
             }
-            if (clients.openWindow) return clients.openWindow('/');
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
         })
     );
 });
